b695dac3ed2548dc5f80e1ecf4ed17e8fb2d4be1
HansKindberg <hans.kindberg@gmail.com>
2020-12-28 14:32:47 +0100

    Support for ui_locales at signout

--- Source/Project/Web/Localization/OpenIdConnectRequestCultureProvider.cs
+++ Source/Project/Web/Localization/OpenIdConnectRequestCultureProvider.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HansKindberg.IdentityServer.Web.Http.Extensions;
+using IdentityServer4.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Primitives;
 
 namespace HansKindberg.IdentityServer.Web.Localization
@@ -13,7 +16,8 @@ namespace HansKindberg.IdentityServer.Web.Localization
 	{
 		#region Properties
 
-		public virtual string QueryStringKey { get; } = QueryStringKeys.UiLocales;
+		protected internal virtual string CultureKey { get; } = QueryStringKeys.UiLocales;
+		protected internal virtual string SignOutKey { get; } = QueryStringKeys.SignOutId;
 
 		#endregion
 
@@ -24,12 +28,47 @@ namespace HansKindberg.IdentityServer.Web.Localization
 			if(httpContext == null)
 				throw new ArgumentNullException(nameof(httpContext));
 
-			var uiLocales = httpContext.Request.Query.GetDistinctSpaceSeparatedValues(this.QueryStringKey);
+			var uiLocales = httpContext.Request.Query.GetDistinctSpaceSeparatedValues(this.CultureKey);
+
+			if(!uiLocales.Any())
+				uiLocales = await this.GetUiLocalesBySignOutAsync(httpContext);
 
 			if(uiLocales.Any())
 				return new ProviderCultureResult(uiLocales.Select(culture => new StringSegment(culture)).ToList());
 
 			return await NullProviderCultureResult;
+		}
+
+		protected internal virtual async Task<IIdentityServerInteractionService> GetInteractionServiceAsync(HttpContext httpContext)
+		{
+			if(httpContext == null)
+				throw new ArgumentNullException(nameof(httpContext));
+
+			return await Task.FromResult(httpContext.RequestServices.GetRequiredService<IIdentityServerInteractionService>());
+		}
+
+		protected internal virtual async Task<ISet<string>> GetUiLocalesBySignOutAsync(HttpContext httpContext)
+		{
+			if(httpContext == null)
+				throw new ArgumentNullException(nameof(httpContext));
+
+			var query = new Dictionary<string, StringValues>(StringComparer.OrdinalIgnoreCase);
+
+			// ReSharper disable InvertIf
+			if(httpContext.Request.Query.ContainsKey(this.SignOutKey))
+			{
+				var interaction = await this.GetInteractionServiceAsync(httpContext);
+
+				var signOutRequest = await interaction.GetLogoutContextAsync(httpContext.Request.Query[this.SignOutKey]);
+
+				var uiLocales = signOutRequest?.Parameters[this.CultureKey];
+
+				if(uiLocales != null)
+					query.Add(this.CultureKey, uiLocales);
+			}
+			// ReSharper restore InvertIf
+
+			return new QueryCollection(query).GetDistinctSpaceSeparatedValues(this.CultureKey);
 		}
 
 		#endregion
