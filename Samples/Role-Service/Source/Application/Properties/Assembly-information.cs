656143866299b9794ae886e73d622cd68a24737c
HansKindberg <hans.kindberg@gmail.com>
2021-02-20 08:21:41 +0100

    Role-Service sample: code-settings

--- Samples/Role-Service/Source/Application/Properties/Assembly-information.cs
+++ Samples/Role-Service/Source/Application/Properties/Assembly-information.cs
@@ -1,4 +1,6 @@
 using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
 
 [assembly: InternalsVisibleTo("Integration-tests")]
 [assembly: InternalsVisibleTo("Unit-tests")]
+[assembly: SupportedOSPlatform("windows")]
