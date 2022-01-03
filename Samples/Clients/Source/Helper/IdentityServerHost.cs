24e7309f2376f0a9be3291f159e40fe803a65172
HansKindberg <hans.kindberg@gmail.com>
2022-01-03 11:43:42 +0100

    Changed Kestrel-port from 44301 to 6001.

--- Samples/Clients/Source/Helper/IdentityServerHost.cs
+++ Samples/Clients/Source/Helper/IdentityServerHost.cs
@@ -5,7 +5,7 @@ namespace Helper
 		#region Fields
 
 		public const string IisAuthority = "https://localhost:44300";
-		public const string KestrelAuthority = "https://localhost:44301";
+		public const string KestrelAuthority = "https://localhost:6001";
 
 		#endregion
 	}
