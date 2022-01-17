0ce2f13ea577fa05e24beeb145cf0182b19c3940
HansKindberg <hans.kindberg@gmail.com>
2022-01-17 21:30:20 +0100

    Removed default-value

--- Source/Project/Saml/Routing/Configuration/ForceAuthenticationRouterOptions.cs
+++ Source/Project/Saml/Routing/Configuration/ForceAuthenticationRouterOptions.cs
@@ -1,12 +1,10 @@
-using HansKindberg.IdentityServer.FeatureManagement;
-
 namespace HansKindberg.IdentityServer.Saml.Routing.Configuration
 {
 	public class ForceAuthenticationRouterOptions
 	{
 		#region Properties
 
-		public virtual string Path { get; set; } = $"/{Feature.ClaimsSelection}";
+		public virtual string Path { get; set; }
 
 		#endregion
 	}
